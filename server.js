25ab142fd35a86c38fb54fde88da3115ac4ae221
arturwiadro <artur.wiadro@gmail.com>
2026-04-10 21:48:59 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "LUMOS backend działa"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server działa na porcie ${port}`);
+});
