f53f95609ef5376824e1dc8a5323719b7340d334
arturwiadro <artur.wiadro@gmail.com>
2026-05-11 19:22:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { Pool } = require("pg");
+const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
